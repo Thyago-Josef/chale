1310fda9568e0b187527a020f55a2612c6cd7cb2
Thyago-Josef <t.y_josef7charm@hotmail.com>
2025-03-19 18:22:19 -0300

    criando navbar

--- chale/settings.py
+++ chale/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Diretório onde o Django procura arquivos estáticos
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Se houver um diretório global 'static/'
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),  # Se houver um diretório global 'static/'
+# ]
 
 # Para produção, os arquivos serão coletados neste diretório
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
